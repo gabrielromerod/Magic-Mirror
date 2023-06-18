a8736f3a7c8707a3d70415eb6a36c1b8ac6cd6ea
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-06-18 02:30:26 -0500

    Mejoramos el modelo de generacion de poemas

--- poema.py
+++ poema.py
@@ -2,11 +2,14 @@ import openai
 
 openai.api_key = "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"
 
-def generar_poema(prompt = "Eres un IA poética que generará poemas que reflexionen sobre la vida de los humanos y cómo a menudo se alejan de vivir sus propias vidas debido a una pantalla. Crea un poema de 20 palabras."):
+def generar_poema(prompt = "Eres una IA poética. Genera un poema profundo y conmovedor que refleje la ironía de los humanos que, atrapados en el encanto de sus pantallas, olvidan vivir y disfrutar sus propias vidas. El poema debe ser de 30 palabras."):
     response = openai.Completion.create(
       engine="text-davinci-003",
       prompt=prompt,
-      temperature=0.7,
+      temperature=0.5,
       max_tokens=100
     )
     return response["choices"][0]["text"].strip() # type: ignore
+
+if __name__ == "__main__":
+    print(generar_poema())

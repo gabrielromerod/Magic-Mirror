af7f7b3f2271afc7101a3eb8e89c06fe9f2ecb29
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-06-21 22:04:20 -0500

    experimentos emocioens

---
+++ emotions.py
@@ -0,0 +1,59 @@
+import cv2
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        face = frame[y:y + h, x:x + w]
+        result = DeepFace.analyze(face, actions=['emotion'], enforce_detection=False)
+
+        if 'dominant_emotion' in result:
+            cv2.putText(frame, result['dominant_emotion'], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+import cv2
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        face = frame[y:y + h, x:x + w]
+        result = DeepFace.analyze(face, actions=['emotion'], enforce_detection=False)
+
+        if 'dominant_emotion' in result:
+            cv2.putText(frame, result['dominant_emotion'], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
